c695fefb3747cbcfa532c7b397ae920d347b5859
dingjingmaster <dingjing@live.cn>
2022-08-22 19:00:01 +0800

    name service

--- app/nameservice/name-service.cpp
+++ app/nameservice/name-service.cpp
@@ -3,3 +3,125 @@
 //
 
 #include "name-service.h"
+
+#include <errno.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+struct _NSPolicyEntry
+{
+    RBNode                          rb;
+    NSPolicy*                       policy;
+    char                            name[1];
+};
+
+int NameService::addPolicy(const char *name, NSPolicy *policy)
+{
+    RBNode** p = &mRoot.rbNode;
+    RBNode* parent = NULL;
+    NSPolicyEntry *entry;
+    int n, ret = -1;
+
+    pthread_rwlock_wrlock(&mRWLock);
+    while (*p) {
+        parent = *p;
+        entry = RB_ENTRY(*p, NSPolicyEntry, rb);
+        n = strcasecmp(name, entry->name);
+        if (n < 0)
+            p = &(*p)->rbLeft;
+        else if (n > 0)
+            p = &(*p)->rbRight;
+        else
+            break;
+    }
+
+    if (!*p) {
+        size_t len = strlen(name);
+        size_t size = offsetof(NSPolicyEntry, name) + len + 1;
+
+        entry = (NSPolicyEntry *)malloc(size);
+        if (entry) {
+            memcpy(entry->name, name, len + 1);
+            entry->policy = policy;
+            rb_link_node(&entry->rb, parent, p);
+            rb_insert_color(&entry->rb, &mRoot);
+            ret = 0;
+        }
+    } else {
+        errno = EEXIST;
+    }
+
+    pthread_rwlock_unlock(&mRWLock);
+
+    return ret;
+}
+
+inline NSPolicyEntry *NameService::getPolicyEntry(const char *name)
+{
+    RBNode* p = mRoot.rbNode;
+    NSPolicyEntry *entry;
+    int n;
+
+    while (p) {
+        entry = RB_ENTRY(p, NSPolicyEntry, rb);
+        n = strcasecmp(name, entry->name);
+        if (n < 0)
+            p = p->rbLeft;
+        else if (n > 0)
+            p = p->rbRight;
+        else
+            return entry;
+    }
+
+    return NULL;
+}
+
+NSPolicy* NameService::getPolicy(const char *name)
+{
+    NSPolicy *policy = mDefaultPolicy;
+    NSPolicyEntry *entry;
+
+    if (mRoot.rbNode) {
+        pthread_rwlock_rdlock(&mRWLock);
+        entry = getPolicyEntry(name);
+        if (entry)
+            policy = entry->policy;
+
+        pthread_rwlock_unlock(&mRWLock);
+    }
+
+    return policy;
+}
+
+NSPolicy* NameService::delPolicy(const char *name)
+{
+    NSPolicy *policy = NULL;
+    NSPolicyEntry *entry;
+
+    pthread_rwlock_wrlock(&mRWLock);
+    entry = getPolicyEntry(name);
+    if (entry)
+    {
+        policy = entry->policy;
+        rb_erase(&entry->rb, &mRoot);
+    }
+
+    pthread_rwlock_unlock(&mRWLock);
+    free(entry);
+
+    return policy;
+}
+
+NameService::~NameService()
+{
+    NSPolicyEntry *entry;
+
+    while (mRoot.rbNode) {
+        entry = RB_ENTRY(mRoot.rbNode, NSPolicyEntry, rb);
+        rb_erase(&entry->rb, &mRoot);
+        free(entry);
+    }
+}
+
--- app/nameservice/name-service.h
+++ app/nameservice/name-service.h
@@ -5,10 +5,137 @@
 #ifndef JARVIS_NAME_SERVICE_H
 #define JARVIS_NAME_SERVICE_H
 
+#include <utility>
+#include <pthread.h>
+#include <functional>
 
-class NameService {
+#include "../factory/task.h"
+#include "../factory/workflow.h"
 
+#include "../core/rb-tree.h"
+#include "../core/communicator.h"
+
+#include "../manager/route-manager.h"
+#include "../manager/endpoint-params.h"
+
+#include "../utils/uri-parser.h"
+
+typedef struct _NSPolicyEntry           NSPolicyEntry;
+
+class RouterTask : public GenericTask
+{
+public:
+    RouteManager::RouteResult *get_result() { return &mResult; }
+
+public:
+    void set_state(int state) { mState = state; }
+    void set_error(int error) { mError = error; }
+
+protected:
+    virtual SubTask *done()
+    {
+        SeriesWork *series = seriesOf(this);
+
+        if (mCallback)
+            mCallback(this);
+
+        delete this;
+        return series->pop();
+    }
+
+public:
+    RouterTask(std::function<void (RouterTask *)>&& cb)
+        : mCallback(std::move(cb))
+    {
+    }
+
+protected:
+    RouteManager::RouteResult                   mResult;
+    std::function<void (RouterTask*)>           mCallback;
 };
 
+class NSTracing
+{
+public:
+    NSTracing()
+    {
+        mData = NULL;
+        mDeleter = NULL;
+    }
+
+public:
+    void*                       mData;
+    void (*mDeleter)(void *);
+};
+
+struct NSParams
+{
+    TransportType               mType;
+    ParsedURI&                  mUri;
+    const char*                 mInfo;
+    bool                        mFixedAddr;
+    int                         mRetryTimes;
+    NSTracing*                  mTracing;
+};
+
+using RouterCallback = std::function<void (RouterTask*)>;
+
+class NSPolicy
+{
+public:
+    virtual RouterTask *create_router_task(const struct WFNSParams *params, RouterCallback callback) = 0;
+
+    virtual void success(RouteManager::RouteResult *result, NSTracing *tracing, CommTarget *target)
+    {
+        RouteManager::notifyAvailable(result->mCookie, target);
+    }
+
+    virtual void failed(RouteManager::RouteResult *result, NSTracing *tracing, CommTarget *target)
+    {
+        if (target)
+            RouteManager::notifyUnavailable(result->mCookie, target);
+    }
+
+public:
+    virtual ~NSPolicy() { }
+};
+
+class NameService
+{
+public:
+    int addPolicy(const char *name, NSPolicy *policy);
+    NSPolicy *getPolicy(const char *name);
+    NSPolicy *delPolicy(const char *name);
+
+public:
+    NSPolicy *getDefaultPolicy() const
+    {
+        return mDefaultPolicy;
+    }
+
+    void set_default_policy(NSPolicy *policy)
+    {
+        mDefaultPolicy = policy;
+    }
+
+
+private:
+    NSPolicyEntry *getPolicyEntry(const char *name);
+
+public:
+    NameService(NSPolicy* defaultPolicy)
+        : mRWLock(PTHREAD_RWLOCK_INITIALIZER)
+    {
+        mRoot.rbNode = NULL;
+        mDefaultPolicy = defaultPolicy;
+    }
+
+    virtual ~NameService();
+
+private:
+    NSPolicy*                   mDefaultPolicy;
+    RBRoot                      mRoot;
+    pthread_rwlock_t            mRWLock;
+};
 
 #endif //JARVIS_NAME_SERVICE_H
